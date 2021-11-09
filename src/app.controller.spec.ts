8e485b789c239ee8cde2b8d29c1475930852c5c6
Pranjali Pokharel <pranjali@ualberta.ca>
2021-11-09 09:41:41 -0700

    Add fail case test scenario

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,5 @@
+import { HttpModule } from '@nestjs/axios';
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +9,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -14,10 +17,19 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return mock home object', () => {
+  describe('home', () => {
+    it('should return home object when getting home by Id', () => {
       const homeData = appController.getHomeById('c573b45f-4dea-4eb5-a2df-54e62414a31c');
       expect(homeData.name).toBe('Our Home');
+    });
+
+    it('should throw a NotFoundException when home not found', () => {
+      expect.assertions(1);
+      try {
+        const missingHomeData = appController.getHomeById('abc_not_a_home');
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException)
+      }
     });
   });
 });
